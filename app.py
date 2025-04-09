8c1e1ca76e51c1cdb4df4f0a9bafa9f4de70c9d2
Varshanth2025 <varshanthg2030@gmail.com>
2025-04-09 22:08:38 +0530

    second commit

--- app.py
+++ app.py
@@ -1,84 +1,54 @@
-from flask import Flask,jsonify,request
+from flask import Flask,jsonify,request,render_template,redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_scss import Scss
+from datetime import datetime
 
-
+#Flask appp setup
 app=Flask(__name__)
+Scss(app)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///drinks_data.db'
 db=SQLAlchemy(app)
 
+#database object-table
 class drinks(db.Model):
     id=db.Column(db.Integer,primary_key=True)
     name=db.Column(db.String(40),unique=True,nullable=False)
-    size=db.Column(db.String(25),nullable=False)
+    size=db.Column(db.String(25),nullable=False) 
+    price=db.Column(db.Integer,default=0)
+    added=db.Column(db.DateTime,default=datetime.utcnow)
 
     def to_dict(self):
        return {
         "id":self.id,
         "name":self.name,
-        "size":self.size
+        "size":self.size,
+        "price":self.price
        }
         
-
-
-with app.app_context():
-    db.create_all()
-
-
-@app.route('/')
+#Home page
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return "Hello! sunny  welcum"
+    if request.method == 'POST':
+        name = request.form['name']
+        size = request.form['size']
+        price = request.form['price']
+        
+        new_data = drinks(name=name, size=size, price=int(price))
+        try:
 
-
-@app.route('/data',methods=['GET'])
-def page1():
-    d=drinks.query.all()
-    return jsonify([drink.to_dict() for drink in d])
-
-
-
-@app.route('/data/<int:id>',methods=['GET'])
-def get_data(id):
-    d=drinks.query.get(id)
-    if d:
-        return   jsonify(d.to_dict())
+            db.session.add(new_data)
+            db.session.commit()
+            return redirect('/')
+        except Exception as e:
+            return f"ERROR:{e}"    
     else:
-        return jsonify({"error":"data not found"}),404
+        data=drinks.query.order_by(drinks.added).all()    
+    return render_template('index.html',data=data)
 
-
-@app.route('/data',methods=['POST'])
-def post_data():
-    data=request.get_json()
-    new=drinks(name=data['name'],size=data['size'])
-    db.session.add(new)
-    db.session.commit()
-    return jsonify(new.to_dict()),201
-
-
-@app.route('/data/<int:id>',methods=['PUT'])
-def put_data(id):
-    data=request.get_json()
-    drink=drinks.query.get(id)
-    if drink:
-        drink.name=data.get('name',drink.name)
-        drink.size=data.get('size',drink.size)
-        db.session.commit()
-        return jsonify(drink.to_dict()),201
-    else:
- 
-        return jsonify({'error':"id not found"}),404
-
-@app.route('/data/<int:id>',methods=['DELETE'])
-def delete_data(id):
-    data=drinks.query.get(id)
-    if data:
-        db.session.delete(data)
-        db.session.commit()
-
-        return jsonify({"message":"delted the data"}),201
-    else:
-        return jsonify({"message":"Id not found"}),404    
 
 if __name__=="__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
 
 
